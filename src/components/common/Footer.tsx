20343f8c5642fb7b9a44a0d76ec3ef6c977e49c4
Adam Hersly <adam.h@gmail.com>
2025-07-21 21:04:53 +0000

    can we right justify it and todays date is July 21 2025

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
-  const version = "0.1-20240718";
+  const version = "0.1-20250721";
   return (
     <footer className="py-4 px-4 text-sm text-muted-foreground border-t flex justify-between items-center">
       <span>Patent Pending by Exitous 2025</span>
