8a81b9dec88b50531475b70f3003153f90744bd7
Adam Hersly <adam.h@gmail.com>
2025-07-25 18:28:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/admin/forms/AdminFormEditor.tsx
+++ src/components/admin/forms/AdminFormEditor.tsx
@@ -1,6 +1,7 @@
 
 
 'use client';
+import * as React from 'react';
 import { useState, useMemo } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { useUserData, Question, buildQuestionTreeFromMap, TaskMapping, MasterTask } from "@/hooks/use-user-data";
