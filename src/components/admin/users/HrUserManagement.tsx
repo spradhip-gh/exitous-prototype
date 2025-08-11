6bc731fe682797be0249217755a5c92a8d23caed
Adam Hersly <adam.h@gmail.com>
2025-08-11 14:33:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/admin/users/HrUserManagement.tsx
+++ src/components/admin/users/HrUserManagement.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 import { useState, useEffect, useRef, useMemo, useCallback } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
